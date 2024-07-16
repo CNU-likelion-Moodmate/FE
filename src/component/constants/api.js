bf0c8ecdac15379ac2e1eb01f096c19faad03730
rlcz1 <rbf0311@gmail.com>
2024-07-17 00:55:45 +0900

    feat: constants

---
+++ src/component/constants/api.js
@@ -0,0 +1,15 @@
+export const USER_URL = {
+  login: '/login',
+  register: '/register',
+};
+
+export const CHAT_URL = {
+  chatting: '/chatting',
+};
+
+export const QUEST_URL = {
+  recommand: '/recommandQuest',
+  save: '/saveQuest',
+  load: '/loadQuest',
+  complete: '/completeQuest', 
+}
---
+++ src/component/constants/color.js
@@ -0,0 +1,15 @@
+export const RED = '#EEB1B2';
+export const YELLOW = '#FEE270';
+export const SKYBLUE = '#BFDBF3';
+export const BLUE = '#719AD0';
+export const PURPLE = '#A399D0';
+
+export const WHITE = '#FFFFFF';
+export const GRAY1 = '#F1F3F5';
+export const GRAY2 = '#E2E2E2';
+export const GRAY3 = '#CCCCCC';
+export const GRAY4 = '#959595';
+export const GRAY5 = '#606060';
+export const BLACK = '#3C3C3C';
+
+export const DIM = 'rgba(0, 0, 0, 0.4)';
