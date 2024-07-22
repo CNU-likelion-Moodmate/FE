46eecf535dfd1cac04e8903e8597abcd35c5a958
yeongju <syj7632@naver.com>
2024-07-22 15:38:36 +0900

    퀘스트 상단바 글자 선

--- src/pages/TopAppBar.jsx
+++ src/pages/TopAppBar.jsx
@@ -3,24 +3,33 @@ import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { URL } from "../../src/component/constants/content";
 import { Div } from "../../src/component/common/div";
-import { GRAY3 } from "../../src/component/constants/color";
+import { BLACK, GRAY3 } from "../../src/component/constants/color";
 import { ChatIcon, QuestIcon, MyPageIcon } from "../../src/assets/icons";
 
 const Container = styled(Div)`
   position: fixed;
   width: 100%;
-  height: 60px;
+  height: 90px;
   justify-content: space-evenly;
   display: flex;
 `;
 
 const LinkButton = styled(Link)`
+  color: ${ props => props.$color || BLACK };
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
   flex-grow: 1;
   font-family: crayon;
+`;
+
+const Underline = styled.div`
+  height: ${ props => props.$height || '4px'};
+  width: ${ props => props.$width || '110%'};
+  border-radius : 8px;
+  background-color: ${ props => props.$color || BLACK };
+  margin-top: 10px;
 `;
 
 const TopAppBar = () => {
@@ -32,12 +41,18 @@ const TopAppBar = () => {
 
   return (
     <Container>
-      <LinkButton to={URL.QUEST}>
+      <div>
+        <LinkButton to={URL.QUEST}>
         추천 받은 퀘스트
-      </LinkButton>
-      <LinkButton to={URL.QUESTROUTINE}>
-        나만의 퀘스트 루틴
-      </LinkButton>
+        <Underline />
+        </LinkButton>
+      </div>
+      <div>
+        <LinkButton to={URL.QUESTROUTINE}>
+          나만의 퀘스트 루틴
+        <Underline />
+        </LinkButton>
+      </div>
     </Container>
   )
 }
