200dd9fd10f59c34cbd5ed8a95aff3c32bc5ca0e
rlcz1 <rbf0311@gmail.com>
2024-08-02 23:36:05 +0900

    build issues

--- src/pages/chat/ChatRoom.jsx
+++ src/pages/chat/ChatRoom.jsx
@@ -94,7 +94,6 @@ const ChatRoom = () => {
     setChatInput('');
     try {
       const res = await chatApi(chat);
-      console.log('chatApi-res', res);
       addChat(res.data.responseChat, false);
     } catch (error) {
       console.error(error);
